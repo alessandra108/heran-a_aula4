39356a8775d39e0597dcfbf0f52224b87414d881
alessandra108 <alessandra10876@gmail.com>
2024-08-22 17:07:12 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -7,11 +7,55 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		
-		ContaComercial cc = new ContaComercial("Ale", 1958, 0, 200);
-		cc.deposito(100);
+		System.out.print("Bem-vindo ao Banco do Itau! ");
 		
-		System.out.println(cc.agua());
-	} 
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("quantas contas voce deseja cadastrar? ");
+        int n = scn.nextInt();
+        
+        for(int i= 1 ; i <= n ; i++) {
+        	System.out.print("digite C para conta corrente ou P para poupança: ");
+        	String x = scn.next();
+        	List<Conta> lista = new ArrayList<>();
+        	
+        	if(x.equals("C")) {
+        		
+        		System.out.print("digite o nome do dono da conta: " + i);
+        		String nome = scn.next();
+        		
+        		System.out.println("digite o numero: ");
+        		int numero = scn.nextInt();
+        		
+        		System.out.println("digite o saldo: ");
+        		double saldo = scn.nextDouble();
+        		
+        		System.out.println("digite o limite: ");
+        		double limite = scn.nextDouble();
+        		
+        		
+        		Conta conta = new Corrente(nome, numero, saldo, limite);
+        		lista.add(conta);
+        	}
+        	else {
+        		System.out.print("digite o nome do dono da conta: " + i);
+        		String nome = scn.next();
+        		
+        		System.out.println("digite o numero: ");
+        		int numero = scn.nextInt();
+        		
+        		System.out.println("digite o saldo: ");
+        		double saldo = scn.nextDouble();
+        		
+  
+        		lista.add( new Poupanca(nome, numero, saldo));
+        		
+        	}
+        	
+        	System.out.println(lista);
 	}
+
+	}
+
 
 
